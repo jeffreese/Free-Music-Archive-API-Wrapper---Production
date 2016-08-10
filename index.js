40679f15300dbd8f808eda108f6ff670b0448e9e
Jeff Reese <thermyte@gmail.com>
2016-08-10 11:02:23 -0600

    configured node server

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT=8080; 
+app.set('port', (process.env.PORT || 5000));
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+app.use(express.static(__dirname + '/app/public'));
 
-//Create a server
-var server = http.createServer(handleRequest);
+app.get('/', function(request, response) {
+  response.render('app/index');
+});
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
